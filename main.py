09bf64490d1e9cb20221d0defe51ad0dd7bd16d6
LEO2822 <mtkashid7@gmail.com>
2025-08-20 19:16:11 +0530

    dev server endpoint added

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Document Converter API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["http://localhost:3000", "https://localhost:3001"],  # Next.js dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
